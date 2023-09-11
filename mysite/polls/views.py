e8aa58ddc39b79f96eb0a7dbdee8eecda851d635
nik443 <nikhram301@gmail.com>
2023-09-11 23:06:34 +1000

    v.2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,9 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 
 def index(request):
     return HttpResponse('Hello world Django documentation')
+
+    
