2e84c4cb58a8adc94d3d9fe781a8c6c680249c11
nik443 <nikhram301@gmail.com>
2023-09-21 08:25:51 +1000

    v.3-4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 
+app_name = 'polls' # пространство имен, добавил для того, чтобы в случае если в других приложения были представления с такими же имена как в этом приложении - не создавалось проблем
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetainView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
